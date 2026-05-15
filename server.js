b8761907f682839b4d2dcf328729c75e4231ffe4
Jack <44131061+ThePixelated@users.noreply.github.com>
2026-05-15 10:08:16 +0700

    change model ai to free (flash)

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/chat', async (req, res) => {
       : message;
 
     const response = await ai.models.generateContent({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-1.5-flash',
       contents: fullPrompt,
     });
 
